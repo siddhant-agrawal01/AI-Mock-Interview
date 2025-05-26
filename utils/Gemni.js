e00b6212b000458fc9e0602ddf9cad8627f1dc8e
siddhant agrawal <siddhant-agrawal01>
2025-05-26 21:45:22 +0530

    added new api

--- utils/Gemni.js
+++ utils/Gemni.js
@@ -6,7 +6,7 @@ const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-pro",
+    model: "gemini-2.0-flash",
 });
 
 const generationConfig = {
